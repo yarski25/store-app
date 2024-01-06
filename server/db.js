05466ea52b2ad77119e9032206791753bca254ba
yarski25 <yaroslav.saprykin@gmail.com>
2024-01-06 17:17:48 +0100

    postgres sql db test connection

--- server/db.js
+++ server/db.js
@@ -1,3 +1,16 @@
-const { Sequilize } = require("sequelize");
+const { Sequelize } = require("sequelize");
 
-module.exports = new Sequilize({});
+module.exports = new Sequelize(
+  `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+);
+
+// module.exports = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     dialect: "postgres",
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//   }
+// );
