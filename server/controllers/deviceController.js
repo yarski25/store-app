6cebfe15b79c9f869329266cf0d7a18badeca090
yarski25 <yaroslav.saprykin@gmail.com>
2024-01-12 00:16:03 +0100

    add brand + device controllers

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,9 +1,49 @@
+const uuid = require("uuid");
+const path = require("path");
+const { Device } = require("../models/models");
+const ApiError = require("../error/apiError");
+
 class DeviceController {
-  async create(req, res) {}
+  async create(req, res, next) {
+    try {
+      const { name, price, brandId, typeId, info } = req.body;
+      const { img } = req.files;
+      let fileName = uuid.v4() + ".jpg";
+      img.mv(path.resolve(__dirname, "..", "static", fileName));
+      const device = await Device.create({
+        name,
+        price,
+        brandId,
+        typeId,
+        info,
+        img: fileName,
+      });
+
+      return res.json(device);
+    } catch (e) {
+      next(ApiError.badRequest(e.message));
+    }
+  }
 
   async getOne(req, res) {}
 
-  async getAll(req, res) {}
+  async getAll(req, res) {
+    const { brandId, typeId } = req.query;
+    let devices;
+    if (!brandId && !typeId) {
+      devices = await Device.findAll();
+    }
+    if (brandId && !typeId) {
+      devices = await Device.findAll({ where: { brandId } });
+    }
+    if (!brandId && typeId) {
+      devices = await Device.findAll({ where: { typeId } });
+    }
+    if (brandId && typeId) {
+      devices = await Device.findAll({ where: { brandId, typeId } });
+    }
+    return res.json(devices);
+  }
 }
 
 module.exports = new DeviceController();
