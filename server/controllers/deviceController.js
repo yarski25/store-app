c042158d2875d02e5d22dad64dedf7c63c726ce3
yarski25 <yaroslav.saprykin@gmail.com>
2024-01-15 19:53:15 +0100

    add user registration

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,23 +1,34 @@
 const uuid = require("uuid");
 const path = require("path");
-const { Device } = require("../models/models");
+const { Device, DeviceInfo } = require("../models/models");
 const ApiError = require("../error/apiError");
 
 class DeviceController {
   async create(req, res, next) {
     try {
-      const { name, price, brandId, typeId, info } = req.body;
+      let { name, price, brandId, typeId, info } = req.body;
       const { img } = req.files;
       let fileName = uuid.v4() + ".jpg";
       img.mv(path.resolve(__dirname, "..", "static", fileName));
+
       const device = await Device.create({
         name,
         price,
         brandId,
         typeId,
-        info,
         img: fileName,
       });
+
+      if (info) {
+        info = JSON.parse(info);
+        info.forEach((i) => {
+          DeviceInfo.create({
+            title: i.title,
+            description: i.description,
+            deviceId: device.id,
+          });
+        });
+      }
 
       return res.json(device);
     } catch (e) {
@@ -25,7 +36,14 @@ class DeviceController {
     }
   }
 
-  async getOne(req, res) {}
+  async getOne(req, res) {
+    const { id } = req.params;
+    const device = await Device.findOne({
+      where: { id },
+      include: [{ model: DeviceInfo, as: "info" }],
+    });
+    return res.json(device);
+  }
 
   async getAll(req, res) {
     let { brandId, typeId, limit, page } = req.query;
@@ -34,16 +52,24 @@ class DeviceController {
     let offset = page * limit - limit;
     let devices;
     if (!brandId && !typeId) {
-      devices = await Device.findAll({ limit, offset });
+      devices = await Device.findAndCountAll({ limit, offset });
     }
     if (brandId && !typeId) {
-      devices = await Device.findAll({ where: { brandId }, limit, offset });
+      devices = await Device.findAndCountAll({
+        where: { brandId },
+        limit,
+        offset,
+      });
     }
     if (!brandId && typeId) {
-      devices = await Device.findAll({ where: { typeId }, limit, offset });
+      devices = await Device.findAndCountAll({
+        where: { typeId },
+        limit,
+        offset,
+      });
     }
     if (brandId && typeId) {
-      devices = await Device.findAll({
+      devices = await Device.findAndCountAll({
         where: { brandId, typeId },
         limit,
         offset,
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,34 @@
 const ApiError = require("../error/apiError");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { User, Basket } = require("../models/models");
+
+const generateJwt = (id, email, role) => {
+  return jwt.sign({ id, email, role }, process.env.SECRET_KEY, {
+    expiresIn: "24h",
+  });
+};
+
 class UserController {
-  async registration(req, res) {}
+  async registration(req, res, next) {
+    const { email, password, role } = req.body;
+    if (!email || !password) {
+      return next(ApiError.badRequest("Incorrect email or password"));
+    }
+    const candidate = await User.findOne({ where: { email } });
+
+    if (candidate) {
+      return next(
+        ApiError.badRequest("User with current email already exists")
+      );
+    }
+
+    const hashPassword = await bcrypt.hash(password, 5);
+    const user = await User.create({ email, password: hashPassword, role });
+    const basket = await Basket.create({ userId: user.id });
+    const token = generateJwt(user.id, user.email, user.role);
+    return res.json(token);
+  }
 
   async login(req, res) {}
 
