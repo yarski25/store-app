f03e4e669c893bc764c4838cab196d6b9cc4c468
yarski25 <yaroslav.saprykin@gmail.com>
2024-01-15 19:05:54 +0100

    add static volume

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -28,19 +28,26 @@ class DeviceController {
   async getOne(req, res) {}
 
   async getAll(req, res) {
-    const { brandId, typeId } = req.query;
+    let { brandId, typeId, limit, page } = req.query;
+    page = page || 1;
+    limit = limit || 9;
+    let offset = page * limit - limit;
     let devices;
     if (!brandId && !typeId) {
-      devices = await Device.findAll();
+      devices = await Device.findAll({ limit, offset });
     }
     if (brandId && !typeId) {
-      devices = await Device.findAll({ where: { brandId } });
+      devices = await Device.findAll({ where: { brandId }, limit, offset });
     }
     if (!brandId && typeId) {
-      devices = await Device.findAll({ where: { typeId } });
+      devices = await Device.findAll({ where: { typeId }, limit, offset });
     }
     if (brandId && typeId) {
-      devices = await Device.findAll({ where: { brandId, typeId } });
+      devices = await Device.findAll({
+        where: { brandId, typeId },
+        limit,
+        offset,
+      });
     }
     return res.json(devices);
   }
