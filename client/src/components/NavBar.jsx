a1431432212f72fd5dad7711add65ac657a1e43f
yarski25 <yaroslav.saprykin@gmail.com>
2024-01-19 18:41:13 +0100

    add nav bar

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,39 @@
+import { useContext } from "react";
+import { Context } from "../main";
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import { SHOP_ROUTE } from "../utils/consts";
+import { observer } from "mobx-react-lite";
+
+const NavBar = observer(() => {
+  const { user } = useContext(Context);
+  console.log(user);
+  return (
+    <Navbar bg="dark" data-bs-theme="dark">
+      <Container fluid className="plr-0">
+        <NavLink style={{ color: "white" }} to={SHOP_ROUTE}>
+          Buy device
+        </NavLink>
+        {user.isAuth ? (
+          <Nav className="ml-auto" style={{ color: "white" }}>
+            <Button variant="outline-light">Admin</Button>
+            <Button variant="outline-light" className="ms-2">
+              Log in
+            </Button>
+          </Nav>
+        ) : (
+          <Nav className="ml-auto" style={{ color: "white" }}>
+            <Button
+              variant="outline-light"
+              onClick={() => user.setIsAuth(true)}
+            >
+              Sign up
+            </Button>
+          </Nav>
+        )}
+      </Container>
+    </Navbar>
+  );
+});
+
+export default NavBar;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,4 +1,4 @@
-import React, { createContext } from "react";
+import { createContext } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import UserStore from "./store/UserStore.js";
@@ -7,14 +7,14 @@ import DeviceStore from "./store/DeviceStore.js";
 export const Context = createContext(null);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Context.Provider
-      value={{
-        user: new UserStore(),
-        device: new DeviceStore(),
-      }}
-    >
-      <App />
-    </Context.Provider>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <Context.Provider
+    value={{
+      user: new UserStore(),
+      device: new DeviceStore(),
+    }}
+  >
+    <App />
+  </Context.Provider>
+  // </React.StrictMode>
 );
