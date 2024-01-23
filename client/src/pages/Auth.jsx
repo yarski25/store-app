db1e842f567bbd7b183a0eb45cf63f7060271486
yarski25 <yaroslav.saprykin@gmail.com>
2024-01-23 18:51:16 +0100

    add signup form

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -1,15 +1,17 @@
 import { Button, Card, Container, Form, Stack } from "react-bootstrap";
-import { NavLink } from "react-router-dom";
-import { REGISTRATION_ROUTE } from "../utils/consts";
+import { NavLink, useLocation } from "react-router-dom";
+import { LOGIN_ROUTE, REGISTRATION_ROUTE } from "../utils/consts";
 
 const Auth = () => {
+  const location = useLocation();
+  const isLogin = location.pathname === LOGIN_ROUTE;
   return (
     <Container
       className="d-flex justify-content-center align-items-center"
       style={{ height: window.innerHeight - 54 }}
     >
       <Card style={{ width: 600 }} className="p-5">
-        <h2 className="m-auto">Login</h2>
+        <h2 className="m-auto">{isLogin ? "Login" : "Signup"}</h2>
         <Form className="d-flex flex-column">
           <Form.Control className="mt-3" placeholder="Insert your email..." />
           <Form.Control
@@ -17,14 +19,27 @@ const Auth = () => {
             placeholder="Insert your password..."
           />
           <Stack className="d-flex flex-row justify-content-between mt-3 pl-3 pr-3">
-            <div className="my-auto">
-              Don&apos;t have an account?{" "}
-              <NavLink to={REGISTRATION_ROUTE}>Sign Up</NavLink>
-            </div>
-
-            {/* <div> */}
-            <Button variant="outline-success">Log In</Button>
-            {/* </div> */}
+            {isLogin ? (
+              <div className="my-auto">
+                Don&apos;t have an account?{" "}
+                <NavLink
+                  style={{ textDecoration: "none" }}
+                  to={REGISTRATION_ROUTE}
+                >
+                  Sign Up
+                </NavLink>
+              </div>
+            ) : (
+              <div className="my-auto">
+                Already have an account?{" "}
+                <NavLink style={{ textDecoration: "none" }} to={LOGIN_ROUTE}>
+                  Log In
+                </NavLink>
+              </div>
+            )}
+            <Button variant="outline-success">
+              {isLogin ? "Log In" : "Sign Up"}
+            </Button>
           </Stack>
         </Form>
       </Card>
