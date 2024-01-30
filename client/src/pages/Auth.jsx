2fd79ea79a866fb9402924a116e04df0a594766c
yarski25 <yaroslav.saprykin@gmail.com>
2024-01-30 17:19:50 +0100

    add api

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -1,10 +1,24 @@
 import { Button, Card, Container, Form, Stack } from "react-bootstrap";
 import { NavLink, useLocation } from "react-router-dom";
 import { LOGIN_ROUTE, REGISTRATION_ROUTE } from "../utils/consts";
+import { login, registration } from "../api/userAPI";
+import { useState } from "react";
 
 const Auth = () => {
   const location = useLocation();
   const isLogin = location.pathname === LOGIN_ROUTE;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const authorize = async () => {
+    if (isLogin) {
+      const response = await login();
+    } else {
+      const response = await registration(email, password);
+      console.log(response);
+    }
+  };
+
   return (
     <Container
       className="d-flex justify-content-center align-items-center"
@@ -13,10 +27,18 @@ const Auth = () => {
       <Card style={{ width: 600 }} className="p-5">
         <h2 className="m-auto">{isLogin ? "Login" : "Signup"}</h2>
         <Form className="d-flex flex-column">
-          <Form.Control className="mt-3" placeholder="Insert your email..." />
+          <Form.Control
+            className="mt-3"
+            placeholder="Insert your email..."
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <Form.Control
             className="mt-3"
             placeholder="Insert your password..."
+            value={password}
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
           />
           <Stack className="d-flex flex-row justify-content-between mt-3 pl-3 pr-3">
             {isLogin ? (
@@ -37,7 +59,7 @@ const Auth = () => {
                 </NavLink>
               </div>
             )}
-            <Button variant="outline-success">
+            <Button onClick={authorize} variant="outline-success">
               {isLogin ? "Log In" : "Sign Up"}
             </Button>
           </Stack>
